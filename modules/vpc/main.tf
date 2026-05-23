e23498b3e93f9cdaa8c7dcccf8ff1866dc289674
Veeresh <appu1engage@gmail.com>
2026-05-23 18:22:11 +0530

    Add VPC module

---
+++ modules/vpc/main.tf
@@ -0,0 +1,40 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+
+tags = {
+    Name = "terraform-vpc"
+}
+}
+
+resource "aws_internet_gateway" "igw"{
+    vpc_id = aws_vpc.main.id
+}
+
+resource "aws_subnet" "public"{
+    count = length(var.public_subnets)
+    vpc_id = aws_vpc.main.id
+    cidr_block = var.public_subnets[count.index]
+    map_public_ip_on_launch = true
+
+availability_zone = element(
+    ["ap-south-1a","ap-south-1b"],
+    count.index
+)
+tags = {
+    Name = "public-subnet-${count.index+1}"
+}
+}
+
+resource "aws_route_table" "public"{
+    vpc_id = aws_vpc.main.id
+}
+resource "aws_route" "internet_access" {
+  route_table_id = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.igw.id
+}
+resource "aws_route_table_association" "public_assoc" {
+  count = length(var.public_subnets)
+  subnet_id = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
